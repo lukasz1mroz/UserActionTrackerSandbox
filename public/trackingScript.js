733a99e9587e64b55da9c6e52435c63b70c2533c
Lukasz Mroz <lukasz.mroz@merck.com>
2022-08-27 14:57:21 +0200

    Add function to access current and stored user visit details

--- public/trackingScript.js
+++ public/trackingScript.js
@@ -1,2 +1,9 @@
-// place your code here
-console.error('tracking script not implemented');
+const getAccessData = async () => {
+  const eventDate = new Date().toISOString();
+  const ipApiResponse = await fetch('https://ipapi.co/json/');
+  const userIp = (await ipApiResponse.json()).ip.toString();
+  // Using localStorage for demo purpose. Data should be stored on server side persistance layer and accessed via API with CRUD methods.
+  const localStorageRecord = JSON.parse(localStorage.getItem(userIp));
+
+  return { eventDate, userIp, localStorageRecord };
+};
